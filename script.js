6a9a1f7b66299d34a78428199bea877bd9812356
Eli0uce <eliouce.gaming@outlook.fr>
2022-10-15 19:43:08 +1100

    update

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let score = 0;
 let health = 10;
 const bullets = [];
 const levelups = [];
+const regens = [];
 const ennemies = [];
 const ennemies2 = [];
 const playerWidth = 100;
@@ -13,10 +14,14 @@ const bulletWidth = 20;
 const bulletHeight = 20;
 const levelupsWidth = 50;
 const levelupsHeight = 50;
+const regensWidth = 50;
+const regensHeight = 50;
 const ennemiesWidth = 80;
 const ennemiesHeight = 80;
 const ennemies2Width = 100;
 const ennemies2Height = 100;
+//const gameWidth = game.style.width;
+//const gameHeight = game.style.height;
 let playerLeft = (window.innerWidth - playerWidth) / 2;
 let playerTop = (window.innerHeight - playerHeight);
 
@@ -40,6 +45,7 @@ window.addEventListener('keydown', function (event) {
     console.log(press);
     if (code == 32) {
         const bullet = this.document.createElement('div');
+        bullet.style.backgroundImage = 'url(./img/bullet1.png)'
         bullet.style.width = bulletWidth + 'px';
         bullet.style.height = bulletHeight + 'px';
         bullet.style.left = parseInt(player.style.left) + ((playerWidth - bulletWidth) / 2) + 'px';
@@ -67,7 +73,6 @@ function generate() {
     ennemy.className = 'ennemy';
     game.appendChild(ennemy);
     ennemies.push(ennemy);
-
     // Generate next ennemy with random time of 5s
     setTimeout(generate, Math.round(Math.random() * speed));
     speed = speed - 50;
@@ -83,13 +88,11 @@ function generate2() {
     ennemy2.className = 'ennemy2';
     game.appendChild(ennemy2);
     ennemies2.push(ennemy2);
-
     // Generate next ennemy with random time of 5s
     setTimeout(generate2, Math.round(Math.random() * speed2));
     speed2 = speed2 - 25;
 
 }
-
 // Generate first ennemy
 generate();
 generate2();
@@ -104,13 +107,27 @@ function levelUp() {
     levelup.className = 'levelup';
     game.appendChild(levelup);
     levelups.push(levelup);
-
     // Generate next bonuses with random time of 30s
     setTimeout(levelUp, Math.round(Math.random() * (10 * 10000)));
 };
-
 // Generate first levelup
 levelUp();
+
+// Function to generate Regen
+function regenUp() {
+    const regen = document.createElement('div')
+    regen.style.width = regensWidth + 'px';
+    regen.style.height = regensHeight + 'px';
+    regen.style.left = Math.round(Math.random() * (window.innerWidth - regensWidth)) + 'px';
+    regen.style.top = 0 + 'px';
+    regen.className = 'regen';
+    game.appendChild(regen);
+    regens.push(regen);
+    // Generate next bonuses with random time of 30s
+    setTimeout(regenUp, Math.round(Math.random() * (10 * 35000)));
+};
+// Generate first levelup
+regenUp();
 
 function draw() {
     if (press && code == 39 && playerLeft <= window.innerWidth - playerWidth) {
@@ -187,35 +204,89 @@ function draw() {
         }
     }
 
-    // Take Bonus(levelup)
-    for (let k = 0; k < levelups.length; k++) {
-        const levelup = levelups[k];
-        for (let l = 0; l < bullets.length; l++) {
-            const bullet = bullets[l];
+    // Spaceship collision 
+    for (let index = 0; index < ennemies.length; index++) {
+        const ennemy = ennemies[index];
+        if (player.offsetLeft + (player.clientWidth / 2) > ennemy.offsetLeft
+            && player.offsetLeft + (player.clientWidth / 2) < ennemy.offsetLeft + ennemy.clientWidth
+            && player.offsetTop + (player.clientHeight / 2) > ennemy.offsetTop
+            && player.offsetTop + (player.clientHeight / 2) < ennemy.offsetTop + ennemy.clientHeight) {
+            game.removeChild(ennemy);
+            ennemies.splice(index, 1);
 
-            if (bullet.offsetLeft + (bullet.clientWidth / 2) > levelup.offsetLeft
-                && bullet.offsetLeft + (bullet.clientWidth / 2) < levelup.offsetLeft + levelup.clientWidth
-                && bullet.offsetTop + (bullet.clientHeight / 2) > levelup.offsetTop
-                && bullet.offsetTop + (bullet.clientHeight / 2) < levelup.offsetTop + levelup.clientWidth) {
-                game.removeChild(levelup);
-                levelups.splice(k, 1);
+            health -= 1;
+            document.getElementById("health").innerHTML = "Santé : " + health;
+            score -= 1;
+            document.getElementById('score').innerHTML = "Score : " + score;
+        }
+    }
 
-                game.removeChild(bullet);
-                bullets.splice(l, 1);
-                score = score + 2;
-                document.getElementById('score').innerHTML = "Score : " + score;
-            }
+    // Spaceship collision 2
+    for (let index = 0; index < ennemies2.length; index++) {
+        const ennemy2 = ennemies2[index];
+        if (player.offsetLeft + (player.clientWidth / 2) > ennemy2.offsetLeft
+            && player.offsetLeft + (player.clientWidth / 2) < ennemy2.offsetLeft + ennemy2.clientWidth
+            && player.offsetTop + (player.clientHeight / 2) > ennemy2.offsetTop
+            && player.offsetTop + (player.clientHeight / 2) < ennemy2.offsetTop + ennemy2.clientHeight) {
+            game.removeChild(ennemy2);
+            ennemies2.splice(index, 1);
+
+            health -= 1;
+            document.getElementById("health").innerHTML = "Santé : " + health;
+            score -= 1;
+            document.getElementById('score').innerHTML = "Score : " + score;
+        }
+    }
+
+    // Take bonus 
+    for (let index = 0; index < levelups.length; index++) {
+        const levelup = levelups[index];
+        if (player.offsetLeft + (player.clientWidth / 2) > levelup.offsetLeft
+            && player.offsetLeft + (player.clientWidth / 2) < levelup.offsetLeft + levelup.clientWidth
+            && player.offsetTop + (player.clientHeight / 2) > levelup.offsetTop
+            && player.offsetTop + (player.clientHeight / 2) < levelup.offsetTop + levelup.clientHeight) {
+            game.removeChild(levelup);
+            levelups.splice(index, 1);
+
+            score += 2;
+            document.getElementById('score').innerHTML = "Score : " + score;
+        }
+    }
+
+    // Take regen 
+    for (let index = 0; index < regens.length; index++) {
+        const regen = regens[index];
+        if (player.offsetLeft + (player.clientWidth / 2) > regen.offsetLeft
+            && player.offsetLeft + (player.clientWidth / 2) < regen.offsetLeft + regen.clientWidth
+            && player.offsetTop + (player.clientHeight / 2) > regen.offsetTop
+            && player.offsetTop + (player.clientHeight / 2) < regen.offsetTop + regen.clientHeight) {
+            game.removeChild(regen);
+            regens.splice(index, 1);
+
+            health += 1;
+            document.getElementById('health').innerHTML = "Santé : " + health;
         }
     }
 
     // Draw levelup
     for (let index = 0; index < levelups.length; index++) {
         const levelup = levelups[index];
-        levelup.style.top = (parseInt(levelup.style.top) + 2) + 'px';
+        levelup.style.top = (parseInt(levelup.style.top) + 1) + 'px';
 
         if (parseInt(levelup.style.top) > window.innerHeight - levelupsHeight) {
             game.removeChild(levelup);
             levelups.splice(index, 1);
+        }
+    }
+
+    // Draw regen
+    for (let index = 0; index < regens.length; index++) {
+        const regen = regens[index];
+        regen.style.top = (parseInt(regen.style.top) + 1) + 'px';
+
+        if (parseInt(regen.style.top) > window.innerHeight - regensHeight) {
+            game.removeChild(regen);
+            regens.splice(index, 1);
         }
     }
 
